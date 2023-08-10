ed95605b41b5617b7c5c1b054b6affe723b0cd90
deyangotsev <deyangotsev10@gmail.com>
2023-08-10 22:15:44 +0300

    New greeting message

--- food_recipes/src/environments/environment.prod.ts
+++ food_recipes/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+
+    apiKey: "AIzaSyBsG3jRvNH4wAsgtYQHf5kMm4SBAKIuPbA",
+
+    authDomain: "myangularapp-19f0e.firebaseapp.com",
+
+    projectId: "myangularapp-19f0e",
+
+    storageBucket: "myangularapp-19f0e.appspot.com",
+
+    messagingSenderId: "424704242050",
+
+    appId: "1:424704242050:web:4ece2fb9591c91f5bb1f9b"
+
+  }
 };
